1978013a6a48ee086ef4e069c5da5f00c81a215f
Geronymos <spielerbereich@gmail.com>
2021-07-10 21:47:24 +0200

    ➕ split in multiple files

---
+++ components/ui/page.py
@@ -0,0 +1,22 @@
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class Page(Gtk.ScrolledWindow):
+    def __init__(self, stack, name):
+        super().__init__()
+        self.name = name
+
+        self.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        self.box = Gtk.FlowBox()
+        self.box.set_valign(Gtk.Align.START)
+        self.box.set_max_children_per_line(3)
+        self.box.set_selection_mode(Gtk.SelectionMode.NONE)
+
+        self.add(self.box)
+        
+        stack.add_titled(self, self.name, self.name)
+
+    def add_to_flowbox(self, widget):
+        self.box.add(widget)
